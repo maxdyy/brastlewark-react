d4cbd35a3efbf948890412104908229c45cd548c
Maksym Dmukhovskyy <dm.maksym@gmail.com>
2019-01-24 17:00:19 +0100

    Updates

---
+++ precache-manifest.c9078a446c8071abce3ed02943c9478f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "54dbb1878c2371092afa",
+    "url": "/brastlewark-react/static/js/runtime~main.54dbb187.js"
+  },
+  {
+    "revision": "079f2bed0b8630922927",
+    "url": "/brastlewark-react/static/js/main.079f2bed.chunk.js"
+  },
+  {
+    "revision": "44563f8c466b2510fe7c",
+    "url": "/brastlewark-react/static/js/1.44563f8c.chunk.js"
+  },
+  {
+    "revision": "079f2bed0b8630922927",
+    "url": "/brastlewark-react/static/css/main.bb7f638a.chunk.css"
+  },
+  {
+    "revision": "44563f8c466b2510fe7c",
+    "url": "/brastlewark-react/static/css/1.25c1880c.chunk.css"
+  },
+  {
+    "revision": "aee69c7884fe90d5183444d3f7a76a53",
+    "url": "/brastlewark-react/index.html"
+  }
+];
